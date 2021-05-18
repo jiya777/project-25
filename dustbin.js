dd8dfae281ad4f662232566dc5efb91ee76e91bf
jiya777 <72973968+jiya777@users.noreply.github.com>
2021-05-18 10:41:14 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,17 @@
+class Dustbin
+{
+    constructor(x, y, width, height) {
+      this.body = Bodies.rectangle(x, y, width, height);
+      this.x=x;
+
+  this.y=y;
+  this.width=200;
+  this.height=213
+  this.thickness=20
+  World.add(world, this.body);
+ 
+
+  
+
+}
+}
